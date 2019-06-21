c5836bcd5255f9bf1b82c79e811b3ed877f2698b
Sunset1234 <emmanuel_burciaga@hotmail.com>
2019-06-21 18:27:13 -0500

    Subiendo Socket App

--- src/app/Servicios/storage-service.service.ts
+++ src/app/Servicios/storage-service.service.ts
@@ -1,15 +1,49 @@
 import { Injectable } from '@angular/core';
 import {HttpClient,HttpHeaders} from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Carta } from '../Modelos/Carta';
 import Ws from '@adonisjs/websocket-client';
+
+// 1Inicializar el socket
+const ws = Ws('ws://localhost:3333');
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageServiceService {
+  
+  // 2 ESA MAMADA ES PARA HACERLO OBSERVABLE
+  private messageSource = new BehaviorSubject([]);
+  currentMessage = this.messageSource.asObservable();
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+
+    //3 CONECTARME AL SOCKET
+    ws.connect();
+    this.MensajeServidor();
+  }
+
+  //-----OBTENER MENSAJE DEL SERVER-----------
+  MensajeServidor(){
+
+    //Subscribirse al canal y al tópico
+    const socket=ws.subscribe('Cartas:1')
+
+    //EN INICIO DEL SOCKET
+    socket.on('ready',()=>{})
+
+    socket.emit('messaje','Hola Mundo')
+
+    //CUANDO SE LLAME AL MESSAJE
+    socket.on('enviar',(messaje)=>{
+      console.log('llegó')
+      this.ChangeMessaje(messaje);
+    })
+  }
+
+  ChangeMessaje(msg){
+    this.messageSource.next(msg)
+  }
 
   //---GET IMAGENES--//
   readonly UrlGetCartas = "http://127.0.0.1:3333/Cartas";
