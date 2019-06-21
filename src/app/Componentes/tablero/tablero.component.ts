863a8e0ce60c75028b7dc5aa135bc9d372114d65
mavericc <escobarveric@gmail.com>
2019-06-21 18:05:59 -0500

    feature: topic en salas

--- src/app/Componentes/tablero/tablero.component.ts
+++ src/app/Componentes/tablero/tablero.component.ts
@@ -1,25 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import Ws from '@adonisjs/websocket-client';
+import { JuegoService } from 'src/app/Servicios/juego.service';
+
 @Component({
   selector: 'app-tablero',
   templateUrl: './tablero.component.html',
   styleUrls: ['./tablero.component.css']
 })
+
 export class TableroComponent implements OnInit {
-  socket= Ws('ws://localhost:3333');
-  datoObtenido: any;
-  constructor() {
-    /*
-    conecta a la url del socket La url del socket es una wea auxilioooooooooojjiiisijfkjrkj
-    */
-    this.socket.connect()
-    const mensaje= this.socket.subscribe('mensaje');
-    mensaje.on('mensaje', valor => {
-      this.datoObtenido= valor
-    });
-   }
+  socket = Ws('ws://localhost:3333');
+  canal: any;
+  room: number;
+  jugador: string;
+  
+  constructor(private juego_service: JuegoService) {
+    //pendiente: ponerle un guard a /tablero
+    this.room = parseInt(localStorage.getItem('juego'));
+
+    this.socket = this.socket.connect();
+    this.canal = this.socket.subscribe('juego:' + this.room);
+
+    this.canal.on('error', data => {
+
+    })
+
+    this.canal.on('entrar', data => {
+      this.jugador = data;
+      setTimeout(() => {
+        
+      }, 1500);
+    })
+  }
 
   ngOnInit() {
+    const room = this.socket.getSubscription('juego:'+ this.room);
+    room.emit('entrar', localStorage.getItem('nick'));
   }
 
 }
