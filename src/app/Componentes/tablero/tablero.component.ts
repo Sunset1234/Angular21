f46e4d259b81af334a007c0350fe570edef1db51
Luis <luis_aguirre_1994@outlook.com>
2019-06-21 19:38:03 -0500

    haré pruebas para la repartición de cartas

--- src/app/Componentes/tablero/tablero.component.ts
+++ src/app/Componentes/tablero/tablero.component.ts
@@ -9,27 +9,11 @@ import { JuegoService } from 'src/app/Servicios/juego.service';
 })
 
 export class TableroComponent implements OnInit {
-<<<<<<< HEAD
-  socket= Ws('ws://localhost:3333');
-  datoObtenido: any;
-
-
-  constructor() {
-    /*
-    conecta a la url del socket La url del socket es una wea auxilioooooooooojjiiisijfkjrkj
-    */
-    this.socket.connect()
-    const mensaje= this.socket.subscribe('mensaje');
-    mensaje.on('mensaje', valor => {
-      this.datoObtenido= valor
-    });
-   }
-=======
   socket = Ws('ws://localhost:3333');
   canal: any;
   room: number;
   jugador: string;
-  
+
   constructor(private juego_service: JuegoService) {
     //pendiente: ponerle un guard a /tablero
     this.room = parseInt(localStorage.getItem('juego'));
@@ -38,17 +22,16 @@ export class TableroComponent implements OnInit {
     this.canal = this.socket.subscribe('juego:' + this.room);
 
     this.canal.on('error', data => {
-
+      console.log('error')
     })
 
     this.canal.on('entrar', data => {
       this.jugador = data;
       setTimeout(() => {
-        
+
       }, 1500);
     })
   }
->>>>>>> room
 
   ngOnInit() {
     const room = this.socket.getSubscription('juego:'+ this.room);
